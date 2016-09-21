678a8322863ef714b7d76f9c98e448560b83de6f
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-09-21 15:35:30 +0300

    implement wrapper function

--- index.js
+++ index.js
@@ -18,7 +18,29 @@ module.exports.thumbnailHuge = function(url){
 }
 
 module.exports.thumbnail = function(url, size){
-	return;
+	size = size || 'm';
+	switch(size){
+		case 's':
+		case 'small':
+			return module.exports.thumbnailSmall(url);
+		case 'b':
+		case 'big':
+			return module.exports.thumbnailBig(url);
+		case 't':
+		case 'smallT':
+			return module.exports.thumbnailSmallT(url);
+		case 'm':
+		case 'medium':
+			return module.exports.thumbnailMedium(url);
+		case 'l':
+		case'large':
+			return module.exports.thumbnailLarge(url);
+		case 'h':
+		case 'huge':
+			return module.exports.thumbnailHuge(url);
+		default:
+			throw new Error('Invalid size argument');
+	}
 }
 
 function addChar(url, char){
