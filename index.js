811d6eb4fc76a36dea09c65b2a0f3b0a501f1af8
Stavros Tsourlidakis <s.tsourlidakis@hotmail.com>
2016-09-21 15:01:21 +0300

    implement basic functions

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+module.exports.thumbnailSmall = function(url){
+	return addChar(url, 's');
+}
+module.exports.thumbnailBig = function(url){
+	return addChar(url, 'b');
+}
+module.exports.thumbnailSmallT = function(url){
+	return addChar(url, 't');
+}
+module.exports.thumbnailMedium = function(url){
+	return addChar(url, 'm');
+}
+module.exports.thumbnailLarge = function(url){
+	return addChar(url, 'l');
+}
+module.exports.thumbnailHuge = function(url){
+	return addChar(url, 'h');
+}
+
 module.exports.thumbnail = function(url, size){
 	return;
+}
+
+function addChar(url, char){
+	const i = url.lastIndexOf('.');
+	if (i != -1) {
+		return url.substr(0, i) + char + url.substr(i) ;
+	} else {
+		throw new Error('Invalid URL');
+	}
 }
